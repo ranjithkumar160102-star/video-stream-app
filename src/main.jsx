c4338b42fcc59cd8e076c1ddc08f25f17e75faf5
[200~ranjithkumar160102-star~ <ranjithkumar160102@gmail.com>
2026-05-16 22:57:12 +0530

    added tailwind config

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
-import { AppProvider } from './context/AppContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
