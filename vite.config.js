e5d24a64cdbdbb8c704ad047192462d3d40a30d4
[200~ranjithkumar160102-star~ <ranjithkumar160102@gmail.com>
2026-05-16 22:46:08 +0530

    fixed netlify blank page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-plugins: [react()],
-base: '/video-stream-app/',
-}) 
+  plugins: [react()],
+})
