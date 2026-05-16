a27cd6d21c7c5a228d612fd3a6de554390b026e3
[200~ranjithkumar160102-star~ <ranjithkumar160102@gmail.com>
2026-05-16 23:16:39 +0530

    fixed final deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-plugins: [react()],
-base: '/video-stream-app/',
-}) 
+  plugins: [react(), tailwindcss()],
+})
